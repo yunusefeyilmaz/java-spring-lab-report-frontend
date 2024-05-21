9a3ed5ba76b2c0189a9f652a263cfe5150a8cbe0
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-21 17:45:59 +0300

    Add home page

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="welcome-screen">
+        <div className="welcome-screen-content">
+            <h1 className="welcome-heading">Welcome to Laboratory Report System</h1>
+            <h3 id="welcome-message">Hello, Yunus</h3>
+            <h3 className="detail-text">You can use navbar to change or add reports.</h3>
+        </div>
+            
+    </div>
+    );
+}
+
+export default Home;
